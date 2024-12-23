767c038f37b81d0b5d0cd3beaede27c8bc699703
S-Toky <stokyr29@gmail.com>
2024-12-23 12:47:26 +0300

    add relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :followers, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followings, class_name: "Relationship", foreign_key: "follower_id"
 end
