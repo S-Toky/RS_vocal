a20edbd4a6076b2ebc08dfdbb788e1ec0984310e
S-Toky <stokyr29@gmail.com>
2024-12-23 12:10:03 +0300

    add like post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :posts
   root to: "home#index"
   devise_for :users
+  resources :posts do
+    resources :comments
+    resources :likes
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
